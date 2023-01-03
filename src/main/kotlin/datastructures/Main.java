9365027ff19cc4818479e229bfee179a4d2d3d5b
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-01-03 20:54:48 +0530

    added code for starting ds

---
+++ src/main/kotlin/datastructures/Main.java
@@ -0,0 +1,18 @@
+package datastructures;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Learning data structures");
+
+        // multiple frameworks of data structures is given in collection framework
+        //you don't have to sit and start implementing those data structures
+        //list
+        //set
+        //map
+
+        //collection -> list -> arraylist,linked list,stack,vector
+        //collection -> set -> enumset, hashset,linkedhashSet, treeset
+        //map -> hashmap,treemap
+    }
+ }
