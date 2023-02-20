8cfb3e0776f7d73da53629580645be93efc6648a
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-02-20 21:42:12 +0530

    Added code for stack data structures

---
+++ src/main/kotlin/stackds/Main.java
@@ -0,0 +1,14 @@
+package stackds;
+
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String args[]) {
+     System.out.println("Learning stack data structure");
+     Vector<Integer> v = new Vector<>();
+     v.add(1);
+     v.add(2);
+     v.add(3);
+    }
+}
