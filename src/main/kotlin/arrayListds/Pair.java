4b71a237e0f725eee715878a51c65c2eb545c8b9
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-02-25 14:39:31 +0530

    Added code for array list

---
+++ src/main/kotlin/arrayListds/Pair.java
@@ -0,0 +1,11 @@
+package arrayListds;
+
+public class Pair<E> {
+    E x;
+    E y;
+
+    public Pair(E x, E y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/kotlin/setds/Pair.java
@@ -0,0 +1,13 @@
+package setds;
+
+public class Pair<E> {
+
+    E x;
+    E y;
+
+    public Pair(E x,E y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
